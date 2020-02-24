9e930827dd2b1055a8930dab2c2420f8b27a8e4f
Robin Beltran <rbeltranmontijo@gmail.com>
2020-02-23 20:24:41 -0600

    iniciando

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => (
+  <div>
+    <h1>Inicio</h1>
+    <style jsx>{`
+      h1 {
+        color: red;
+      }
+    `}</style>
+  </div>
+);
+
+export default Home;
