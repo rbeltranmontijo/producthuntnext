fff5e8b8c27eaac699dde578946f479b7816efd7
Robin Beltran <rbeltranmontijo@gmail.com>
2020-02-29 12:24:27 -0600

    estilos de crear cuenta

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,12 +1,51 @@
-import Layout from "../components/layout/Layout";
 import React from "react";
+import { css } from "@emotion/core";
+import Layout from "../components/layout/Layout";
+import { Formulario, Campo, InputSubmit } from "../components/ui/Formulario";
 
 const CrearCuenta = () => {
   return (
     <div>
       <Layout>
         <>
-          <h1>Crear cuenta</h1>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >
+            Crear cuenta
+          </h1>
+          <Formulario>
+            <Campo>
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                id="nombre"
+                placeholder="Tu Nombre"
+                name="nombre"
+              />
+            </Campo>
+            <Campo>
+              <label htmlFor="nombre">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Tu email"
+                name="email"
+              />
+            </Campo>
+            <Campo>
+              <label htmlFor="nombre">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Tu password"
+                name="password"
+              />
+            </Campo>
+            <InputSubmit type="submit" value="Crear Cuenta" />
+          </Formulario>
         </>
       </Layout>
     </div>
