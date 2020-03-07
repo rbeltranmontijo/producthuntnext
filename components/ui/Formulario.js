cdc5b0e3db54f0e39c02542640855fb605fbc1d3
Robin Beltran <rbeltranmontijo@gmail.com>
2020-03-07 13:18:57 -0600

    pagina nuevo-producto

--- components/ui/Formulario.js
+++ components/ui/Formulario.js
@@ -4,6 +4,11 @@ export const Formulario = styled.form`
   max-width: 600px;
   width: 95%;
   margin: 5rem auto 0 auto;
+  fieldset {
+    margin 2rem 0;
+    border: 1px solid #e1e1e1;
+    padding: 2rem
+  }
 `;
 
 export const Campo = styled.div`
@@ -14,9 +19,13 @@ export const Campo = styled.div`
     flex: 0 0 150px;
     font-size: 1.8rem;
   }
-  input {
+  input,
+  textarea {
     flex: 1;
     padding: 1rem;
+  }
+  textarea {
+    height: 400px;
   }
 `;
 
--- pages/nuevo-producto.js
+++ pages/nuevo-producto.js
@@ -1,11 +1,116 @@
+import React, { useState } from "react";
+import Router from "next/router";
+import { css } from "@emotion/core";
 import Layout from "../components/layout/Layout";
-import React from "react";
+import {
+  Formulario,
+  Campo,
+  InputSubmit,
+  Error
+} from "../components/ui/Formulario";
+
+import firebase from "../firebase";
+
+// Validaciones
+import useValidacion from "../hooks/useValidacion";
+import validarCrearCuenta from "../validacion/validarCrearCuenta";
+
+const STATE_INICIAL = {
+  nombre: "",
+  empresa: "",
+  imagen: "",
+  url: "",
+  descripcion: ""
+};
 
 const NuevoProducto = () => {
+  const [error, guardarError] = useState(false);
+  const {
+    valores,
+    errores,
+    handleSubmit,
+    handleChange,
+    handleBlur
+  } = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
+  const { nombre, empresa, imagen, url, descripcion } = valores;
+
+  async function crearCuenta() {}
+
   return (
     <div>
       <Layout>
-        <h1>Nuevo producto</h1>
+        <>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >
+            Nuevo Producto
+          </h1>
+          <Formulario onSubmit={handleSubmit}>
+            <fieldset>
+              <legend>Informacion General</legend>
+
+              <Campo>
+                <label htmlFor="nombre">Nombre</label>
+                <input
+                  type="text"
+                  id="nombre"
+                  placeholder="Tu Nombre"
+                  name="nombre"
+                  value={nombre}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+              {errores.nombre && <Error>{errores.nombre}</Error>}
+
+              <Campo>
+                <label htmlFor="imagen">Imagen</label>
+                <input
+                  type="text"
+                  id="imagen"
+                  name="imagen"
+                  value={imagen}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+              {errores.imagen && <Error>{errores.imagen}</Error>}
+
+              <Campo>
+                <label htmlFor="url">Url</label>
+                <input
+                  type="url"
+                  id="url"
+                  name="url"
+                  value={url}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+              {errores.url && <Error>{errores.url}</Error>}
+            </fieldset>
+
+            <fieldset>
+              <legend>Sobre tu producto</legend>
+              <Campo>
+                <label htmlFor="descripcion">Descripcion</label>
+                <textarea
+                  id="descripcion"
+                  name="descripcion"
+                  value={descripcion}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+              {errores.descripcion && <Error>{errores.descripcion}</Error>}
+            </fieldset>
+
+            <InputSubmit type="submit" value="Crear Producto" />
+          </Formulario>
+        </>
       </Layout>
     </div>
   );
