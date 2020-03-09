3e99d70f91bd48b598764b70f91ee42610c26ff8
Robin Beltran <rbeltranmontijo@gmail.com>
2020-03-08 21:03:27 -0600

    obteniendo la lista de los productos

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
 import Layout from "../components/layout/Layout";
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { FirebaseContext } from "../firebase";
 
 const Home = () => {
+  const [productos, guardarProductos] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db
+        .collection("productos")
+        .orderBy("creado", "desc")
+        .onSnapshot(manejarSnapshot);
+    };
+    obtenerProductos();
+  }, []);
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      };
+    });
+    guardarProductos(productos);
+  }
   return (
     <div>
       <Layout>
