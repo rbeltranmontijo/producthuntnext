87958578e37331c8118593cf502131e506310e84
Robin Beltran <rbeltranmontijo@gmail.com>
2020-03-08 21:58:13 -0600

    componente de DetallesProducto dentro de home para listar cada uno de los productos

---
+++ components/layout/DetallesProducto.js
@@ -0,0 +1,124 @@
+import React from "react";
+import styled from "@emotion/styled";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import { es } from "date-fns/locale";
+
+const Producto = styled.li`
+  padding: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #e1e1e1;
+`;
+
+const Imagen = styled.img`
+  width: 200px;
+`;
+
+const DescripcionProducto = styled.div`
+  flex: 0 1 600px;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  column-gap: 2rem;
+`;
+
+const Titulo = styled.a`
+  font-size: 2rem;
+  font-weight: bold;
+  margin: 0;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const TextoDescripcion = styled.p`
+  font-size: 2rem;
+  margin: 0;
+  color: #888;
+`;
+
+const Comentarios = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  align-items: center;
+  div {
+    display: flex;
+    align-items: center;
+    border: 1px solid #e1e1e1;
+    padding: 0.3rem 1rem;
+    margin-right: 2rem;
+  }
+  img {
+    width: 2rem;
+    margin-right: 2rem;
+  }
+  p {
+    font-size: 1.6rem;
+    margin-right: 1rem;
+    font-weight: 700;
+    &last-of-type {
+      margin: 0;
+    }
+  }
+`;
+
+const Votos = styled.div`
+  flex: 0 0 auto;
+  text-align: center;
+  border: 1px solid #e1e1e1;
+  padding: 1rem 3rem;
+
+  div {
+    font-size: 2rem;
+  }
+
+  p {
+    margin: 0;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+`;
+
+const DetallesProducto = ({ producto }) => {
+  const {
+    id,
+    nombre,
+    comentarios,
+    creado,
+    descripcion,
+    empresa,
+    url,
+    urlImagen,
+    votos
+  } = producto;
+  return (
+    <Producto>
+      <DescripcionProducto>
+        <div>
+          <Imagen src={urlImagen} alt="" />
+        </div>
+        <div>
+          <Titulo>{nombre}</Titulo>
+          <TextoDescripcion>{descripcion}</TextoDescripcion>
+          <Comentarios>
+            <div>
+              <img src="/static/img/comentario.png" alt="" />
+              <p>{comentarios.length} Comentarios</p>
+            </div>
+          </Comentarios>
+          <p>
+            Publicado hace:{" "}
+            {formatDistanceToNow(new Date(creado), { locale: es })}
+          </p>
+        </div>
+      </DescripcionProducto>
+      <Votos>
+        <div>&#9650;</div>
+        <p>{votos}</p>
+      </Votos>
+    </Producto>
+  );
+};
+
+export default DetallesProducto;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from "../components/layout/Layout";
 import React, { useEffect, useState, useContext } from "react";
+import DetallesProducto from "../components/layout/DetallesProducto";
 import { FirebaseContext } from "../firebase";
 
 const Home = () => {
@@ -26,7 +27,15 @@ const Home = () => {
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map(producto => (
+                <DetallesProducto key={producto.id} producto={producto} />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   );
